fef894aee79141218dd93fcfe0e13e5c736f8523
hoang <hoang@r48p.kist>
2026-01-10 18:13:05 +0900

    feats: added logic and test of logic

---
+++ src/app/logic.py
@@ -0,0 +1,32 @@
+import joblib
+import pandas as pd
+import xgboost as xgb
+from dataclasses import dataclass
+
+@dataclass
+class ModelConfig:
+    model_path: str
+    le_source_path: str
+    le_category_path: str
+
+def load_model(config: ModelConfig):
+    # Load model XGBoost 
+    model = xgb.XGBRegressor()
+    model.load_model(config.model_path)
+
+    # Load label encoders
+    le_category = joblib.load(config.le_category_path)
+    le_source = joblib.load(config.le_source_path)
+
+    return model, le_category, le_source
+
+def predict_demand(model: xgb.XGBRegressor, price: float, category_id: int, source_id: int, day_of_week: int):
+    # Tạo input data
+    input_data = pd.DataFrame(dict(
+        avg_price=[price], category_encoded=[category_id], source_encoded=[source_id], day_of_week=[day_of_week]
+    ))
+
+    # Predict
+    pred = model.predict(input_data)[0]
+    return max(0.0, float(pred)) # Ko nên có demand âm
+
---
+++ tests/test_logic.py
@@ -0,0 +1,34 @@
+import pytest
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+from app import ModelConfig
+from app import logic
+
+@pytest.fixture
+def model_config():
+    return ModelConfig(
+        model_path="models/best_model.json",
+        le_source_path="models/source_encoder.pkl",
+        le_category_path="models/category_encoder.pkl",
+    )
+
+def test_load_model(model_config):
+    model, le_category, le_source = logic.load_model(model_config)
+
+    assert type(model) == xgb.XGBRegressor
+    assert type(le_category) == LabelEncoder
+    assert type(le_source) == LabelEncoder
+
+def test_predict_demand(model_config):
+    model, _, _ = logic.load_model(model_config)
+
+    pred_demand = logic.predict_demand(
+        model=model,
+        price=23.99,
+        category_id=0,
+        source_id=1,
+        day_of_week=2
+    )
+
+    assert type(pred_demand) == float
+    assert pred_demand >= 0
